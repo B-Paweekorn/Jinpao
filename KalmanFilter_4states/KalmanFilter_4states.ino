432f73e43dc33e43da7e76f4c7ed512a6de1947c
tuchapong1234 <tuchapong1234@gmail.com>
2023-12-28 03:27:52 +0700

    Final Class Kalman Filter

--- KalmanFilter_4states/KalmanFilter_4states.ino
+++ KalmanFilter_4states/KalmanFilter_4states.ino
@@ -10,25 +10,11 @@ AS5600 as5600;  //  use default Wire
 #include "KalmanFilter.h"
 /* USER CODE END Includes */
 
-float frequency;
-float dutyCycle;
-
 /* PREPARE kalman filter BEGIN*/
 
 // declare kalman filter instance
 kalman_filter KF;
 
-//known input
-float U_data[1] = { 0.0 };
-matrix U_MATRIX(1, 1, U_data);
-// Y measurement
-float Ymeas_data[1] = { 0.0 };
-matrix Ymeas_MATRIX(1, 1, Ymeas_data);
-//output
-matrix KF_output(3, 1);
-
-float newU[1] = { 0 };
-float newYmeas[1] = { 0 };
 /* PREPARE kalman filter END*/
 
 //------------------------------------
@@ -86,7 +72,13 @@ unsigned long tcur;
 int rawAngle_prev = 0;
 int count = 0;
 int32_t Angle = 0;
+int32_t Angle_rad = 0;
 float velocity_angle = 0;
+//Input Command
+float Vin;
+
+//Output of Kalmanfilter
+float estimateVel;
 
 //------------------------------------
 /****    SIMULATOR PARAMETERS   ****/
@@ -101,43 +93,6 @@ float velocity_angle = 0;
 float dummy_sinwave = 0;
 #define SIMUL_PERIOD 0.3  // oscillating period [s]
 #define SIMUL_AMP 4900.0  // oscillation amplitude
-
-// state trnsition matrix
-float A_data[16] = { 1.0, 9.945211490311812E-4, -3.727956981163958E-5, 4.075838177204351E-6,
-                    0.0, 0.98685183868228, -0.074386312650263, 0.005453127455664,
-                    0.0, 0.0, 1.0, 0.0,
-                    0.0, -0.79388648772862, 0.044382187370876, 0.027207395024798 };
-matrix A_MATRIX(4, 4, A_data);
-
-//input matrix
-float B_data[4] = { 9.012107076660543E-06,
-                    0.022191093685438,
-                    0.0,
-                    1.509516246505134 };
-matrix B_MATRIX(4, 1, B_data);
-
-// observation matrix
-float C_data[4] = { 1.0, 0.0, 0.0, 0.0};
-matrix C_MATRIX(1, 4, C_data);
-
-//D matrix
-matrix D_MATRIX(4, 1);
-
-// measurement covariance matrix
-float R_data[1] = { 10000 }; //0.09
-matrix R_MATRIX(1, 1, R_data);
-
-// Process noise covariance matrix
-float G_data[4] = { 0.0, 1.0, 0.0, 0.0 };
-matrix G_MATRIX(4, 1, G_data);
-// float Q_data[9] = { m_p * m_p, 0.0, 0.0,
-//                         0.0, m_v * m_v, 0.0,
-//                         0.0, 0.0, m_i * m_i };
-// matrix Q_MATRIX(3, 3, &Q_data);
-
-//process model variance vaule
-float Q_data[1] = { 0.01 };  //assign Q here<------------- 0.01
-matrix Q_MATRIX(1, 1, Q_data);
 
 
 //------------------------------------
@@ -159,33 +114,6 @@ void setup() {
   Serial.print("Connect: ");
   Serial.println(b);
   delay(1000);
-
-  // // time evolution matrix (whatever... it will be updated inloop)
-  // K.F = { 1.0, 9.945211490311812E-4, 4.075838177204351E-6,
-  //         0.0, 0.98685183868228, 0.005453127455664,
-  //         0.0, -0.79388648772862, 0.027207395024798 };
-
-  // // Input matrix
-  // K.B = { 9.012107076660543e-06,
-  //         0.022191093685438,
-  //         1.509516246505134};
-
-  // // measurement matrix n the position and velocity
-  // K.H = { 1.0, 0.0, 0.0};
-
-  // // measurement covariance matrix
-  // K.R = { n_p * n_p};
-
-  // // model covariance matrix
-  // K.Q = { m_p * m_p, 0.0, 0.0,
-  //       0.0, m_v * m_v, 0.0,
-  //       0.0, 0.0, m_i * m_i };
-  /*kalman filter INITIAL BEGIN*/
-
-  //kalman filter matrix set up
-  KF.setAtoD(A_MATRIX, B_MATRIX, C_MATRIX, D_MATRIX);
-  KF.setQGR(Q_MATRIX, G_MATRIX, R_MATRIX);
-  /*kalman filter INITIAL END*/
   
   //PWM Pin
   ledcAttach(pwmPin, PWM1_Freq, PWM1_Res);
@@ -194,6 +122,7 @@ void setup() {
   //Direction Pin
   pinMode(dirPin, OUTPUT);
   
+  KF.init();
 }
 
 void loop() {
@@ -210,48 +139,9 @@ void loop() {
   if (current_timestep_print - timestamp_print > timestep_print) {
     timestamp_print = micros();
 
-    // Serial.print(15000);
-    // Serial.print(" ");
-    // Serial.print(-15000);
-    // Serial.print(" ");
-    // Serial.println(dummy_sinwave);
-
-    Serial.print(newYmeas[0]);
-    Serial.print(" ");
     Serial.print(velocity_angle);
     Serial.print(" ");
-    Serial.print(KF_output.data[0][0]);
-    Serial.print(" ");
-    Serial.println(KF_output.data[1][0]);
-    // Serial.print(" ");
-    // Serial.print(KF.gainK.data[0][0]);
-    // Serial.print(" ");
-    // Serial.print(KF.errorY.data[0][0]);
-    // Serial.print(" ");
-    // Serial.print(KF.Y.data[0][0]);
-    // Serial.print(" ");
-    // Serial.print(KF.predictX_old.data[0][0]);
-    // Serial.print(" ");
-    // Serial.print(KF.P_new.data[0][0]);
-    // Serial.print(" ");
-    // Serial.print(KF.P_new.data[0][1]);
-    // Serial.print(" ");
-    // Serial.print(KF.P_new.data[0][2]);
-    // Serial.print(" ");
-    // Serial.print(KF.P_new.data[1][0]);
-    // Serial.print(" ");
-    // Serial.print(KF.P_new.data[1][1]);
-    // Serial.print(" ");
-    // Serial.print(KF.P_new.data[1][2]);
-    // Serial.print(" ");
-    // Serial.print(KF.P_new.data[2][0]);
-    // Serial.print(" ");
-    // Serial.print(KF.P_new.data[2][1]);
-    // Serial.print(" ");
-    // Serial.println(KF.P_new.data[2][2]);
-    
-    // Serial.print(" ");
-    // Serial.println(-35);
+    Serial.println(estimateVel);
   }
 
   //Control loop
@@ -263,17 +153,11 @@ void loop() {
     // Generate Sinwave to motor
     setmotor_sinwave();
 
-    // Update Input into com
-    
-
-    // // Result of the measurement is written into 'obs'
+    // Result of the measurement is written into 'obs'
     Update_Measurement();
 
-    // // APPLY KALMAN FILTER
-    U_MATRIX.read(newU);
-    Ymeas_MATRIX.read(newYmeas);
-    KF.run(U_MATRIX, Ymeas_MATRIX);
-    KF_output = KF.resultX;
+    estimateVel = KF.EstimateSpeed(Angle, Vin);
+
   }
 }
 
@@ -286,6 +170,7 @@ void setmotor_sinwave() {
   dummy_sinwave = SIMUL_AMP * sin(tcur / 1000.0 / SIMUL_PERIOD);  // position
   //dummy_sinwave = SIMUL_AMP/SIMUL_PERIOD*cos(tcur/1000.0/SIMUL_PERIOD); // speed
 
+  // Update Input into com
   Update_Command(dummy_sinwave);
 
   if (dummy_sinwave >= 0) {
@@ -312,15 +197,12 @@ int AS5600_Unwrap(int rawAngle) {
   return count * 4096 + rawAngle;
 }
 
-
-
 void Update_Command(int Raw_dutyCycle) {
-  float Vin = Raw_dutyCycle * 18 / 16383.0;  // 14 Bits (16383)
-  newU[0] = Vin;
+  Vin = Raw_dutyCycle * 18 / 16383.0;  // 14 Bits (16383)
 }
 
 void Update_Measurement() {
   Angle = AS5600_Unwrap(as5600.rawAngle());                      //Unwrap Angle
-  newYmeas[0] = Angle * AS5600_RAW_TO_RADIANS;                   //Position
+  Angle_rad = Angle * AS5600_RAW_TO_RADIANS;                   //Position
   velocity_angle = as5600.getAngularSpeed(AS5600_MODE_RADIANS);  //Velocity
 }
