1809ea0c16a4f38846b95844b3bd4f96dde87866
Fzil0n <tanawat84598@gmail.com>
2023-12-27 18:15:57 +0700

    add matrix and kalman class

--- KalmanFilter_4states/KalmanFilter_4states.ino
+++ KalmanFilter_4states/KalmanFilter_4states.ino
@@ -2,11 +2,18 @@
 #include "Wire.h"
 AS5600 as5600;  //  use default Wire
 
-#include <Kalman.h>
-using namespace BLA;
+// #include <Kalman.h>
+// using namespace BLA;
 
+/* USER CODE BEGIN Includes */
+#include "Matrix.h"
+#include "KalmanFilter.h"
+/* USER CODE END Includes */
 float frequency;
 float dutyCycle;
+
+// declare kalman filter instance
+kalman_filter KF();
 
 //------------------------------------
 /****  MICROCONTROLLER SETUP  ****/
@@ -22,23 +29,21 @@ float dutyCycle;
 /****  MODELIZATION PARAMETERS  ****/
 //------------------------------------
 
-#define Nstate 4  // Position, Velocity, External Load, Current
-#define Nobs 2    // Position, Velocity
-#define Ncom 2    // Vin, External Force
+#define Nstate 3  // Position, Velocity, Current
+#define Nobs 1    // Position
+#define Ncom 1    // Vin
 
 // measurement std of the noise
 #define n_p 0.3         // position measurement noise
-#define n_v 10000000.0  // velocity measurement noise
 
 // model std (1/inertia)
 #define m_p 0.1  // position model noise
 #define m_v 0.1  // velocity model noise
-#define m_l 0.8  // external force model noise
 #define m_i 0.8  // current model noise
 
-BLA::Matrix<Nobs> obs;         // observation vector
-BLA::Matrix<Ncom> com;         // command vector (input)
-KALMAN<Nstate, Nobs, Ncom> K;  // your Kalman filter
+// BLA::Matrix<Nobs> obs;         // observation vector
+// BLA::Matrix<Ncom> com;         // command vector (input)
+// KALMAN<Nstate, Nobs, Ncom> K;  // your Kalman filter
 // Note: I made 'obs' a global variable so memory is allocated before the loop.
 //       This might provide slightly better speed efficiency in loop.
 
@@ -73,7 +78,7 @@ int32_t Angle = 0;
 // These variables simulate a physical process to be measured
 // In real life, the SIMULATOR is replaced by your operational system
 
-BLA::Matrix<Nstate> state;  // true state vector
+// BLA::Matrix<Nstate> state;  // true state vector
 
 //Generate Sinwave
 float dummy_sinwave = 0;
@@ -101,38 +106,62 @@ void setup() {
   Serial.println(b);
   delay(1000);
 
-  // time evolution matrix (whatever... it will be updated inloop)
-  K.F = { 1.0, 9.945211490311812E-4, -3.727956981163958E-5, 4.075838177204351E-6,
-          0.0, 0.986851838682282, -0.074386312650263, 0.005453127455664,
-          0.0, 0.0, 1.0, 0.0,
-          0.0, -0.793886487728624, 0.044382187370876, 0.027207395024798 };
+  // // time evolution matrix (whatever... it will be updated inloop)
+  // K.F = { 1.0, 9.945211490311812E-4, 4.075838177204351E-6,
+  //         0.0, 0.98685183868228, 0.005453127455664,
+  //         0.0, -0.79388648772862, 0.027207395024798 };
 
-  // Input matrix
-  K.B = { 9.012107076660543e-06, 0.0,
-          0.022191093685438, 0.0,
-          0.0, 0.0,
-          1.509516246505134, 0.0 };
+  // // Input matrix
+  // K.B = { 9.012107076660543e-06,
+  //         0.022191093685438, 
+  //         1.509516246505134};
 
-  // measurement matrix n the position and velocity
-  K.H = { 1.0, 0.0, 0.0, 0.0,
-          0.0, 0.0, 0.0, 0.0 };
+  // // measurement matrix n the position and velocity
+  // K.H = { 1.0, 0.0, 0.0};
+
+  // // measurement covariance matrix
+  // K.R = { n_p * n_p};
+
+  // // model covariance matrix
+  // K.Q = { m_p * m_p, 0.0, 0.0,
+  //       0.0, m_v * m_v, 0.0,
+  //       0.0, 0.0, m_i * m_i };
+  // state trnsition matrix
+  float A_data[9]  = { 1.0, 9.945211490311812E-4, 4.075838177204351E-6,
+                        0.0, 0.98685183868228, 0.005453127455664,
+                        0.0, -0.79388648772862, 0.027207395024798 };
+  matrix A_MATRIX(3, 3, &A_data);
+
+  //input matrix
+  float B_data[3]  = { 9.012107076660543E-06,
+                           0.022191093685438, 
+                           1.509516246505134};
+  matrix B_MATRIX(3, 1, &B_data);
+
+  // observation matrix
+  float C_data[3] = { 1.0, 0.0, 0.0};
+  matrix C_MATRIX(1, 3, &C_data);
+  
+  //D matrix
+  matrix D_MATRIX(3, 1)
 
   // measurement covariance matrix
-  K.R = { n_p * n_p, 0.0,
-          0.0, n_v * n_v };
+  float R_data[1] = { n_p * n_p};
+  matrix R_MATRIX(1, 1, &R_data);
 
-  // model covariance matrix
-  K.Q = { m_p * m_p, 0.0, 0.0, 0.0,
-          0.0, m_v * m_v, 0.0, 0.0,
-          0.0, 0.0, m_l * m_l, 0.0,
-          0.0, 0.0, 0.0, m_i * m_i };
-
+  // Process noise covariance matrix
+  float Q_data[9] = { m_p * m_p, 0.0, 0.0,
+                          0.0, m_v * m_v, 0.0,
+                          0.0, 0.0, m_i * m_i };
+  matrix Q_MATRIX(3, 3, &Q_data);
+  //kalman filter matrix set up
+  KF.setAtoD(A_MATRIX, B_MATRIX, C_MATRIX, )
   //Direction Pin
   pinMode(dirPin, OUTPUT);
 
   //PWM Pin
-  ledcAttachPin(pwmPin, PWM1_Ch);
-  ledcSetup(PWM1_Ch, PWM1_Freq, PWM1_Res);
+  // ledcAttachPin(pwmPin, PWM1_Ch);
+  // ledcSetup(PWM1_Ch, PWM1_Freq, PWM1_Res);
 }
 
 void loop() {
@@ -155,17 +184,17 @@ void loop() {
     // Serial.print(" ");
     // Serial.println(dummy_sinwave);
 
-    Serial.print(obs(0));
-    Serial.print(" ");
-    Serial.print(obs(1));
-    Serial.print(" ");
-    Serial.print(K.x(0));
-    Serial.print(" ");
-    Serial.print(K.x(1));
-    Serial.print(" ");
-    Serial.print(35);
-    Serial.print(" ");
-    Serial.println(-35);
+    // Serial.print(obs(0));
+    // Serial.print(" ");
+    // Serial.print(obs(1));
+    // Serial.print(" ");
+    // Serial.print(K.x(0));
+    // Serial.print(" ");
+    // Serial.print(K.x(1));
+    // Serial.print(" ");
+    // Serial.print(35);
+    // Serial.print(" ");
+    // Serial.println(-35);
   }
 
   //Control loop
@@ -181,10 +210,10 @@ void loop() {
     //Update_Command(dummy_sinwave);
 
     // // Result of the measurement is written into 'obs'
-    Update_Measurement();
+    // Update_Measurement();
 
     // // APPLY KALMAN FILTER
-    K.update(obs, com);
+    // K.update(obs, com);
   }
 }
 
@@ -221,13 +250,13 @@ int AS5600_Unwrap(int rawAngle) {
   return count * 4096 + rawAngle;
 }
 
-void Update_Command(int Raw_dutyCycle) {
-  float Vin = Raw_dutyCycle * 18 / 100.0;  // 14 Bits (16383)
-  com(0) = Vin;
-}
+// void Update_Command(int Raw_dutyCycle) {
+//   float Vin = Raw_dutyCycle * 18 / 100.0;  // 14 Bits (16383)
+//   com(0) = Vin;
+// }
 
-void Update_Measurement() {
-  Angle = AS5600_Unwrap(as5600.rawAngle());              //Unwrap Angle
-  obs(0) = Angle * AS5600_RAW_TO_RADIANS;                //Position
-  obs(1) = as5600.getAngularSpeed(AS5600_MODE_RADIANS);  //Velocity
-}
+// void Update_Measurement() {
+//   Angle = AS5600_Unwrap(as5600.rawAngle());              //Unwrap Angle
+//   obs(0) = Angle * AS5600_RAW_TO_RADIANS;                //Position
+//   obs(1) = as5600.getAngularSpeed(AS5600_MODE_RADIANS);  //Velocity
+// }
